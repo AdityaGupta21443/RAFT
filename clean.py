e9fc070af925b8ddb9d3c6f601a9d115e07d32f9
biscuitdrinker <aditya21443@iiitd.ac.in>
2024-04-03 13:00:17 +0530

    adding clean.py

---
+++ clean.py
@@ -0,0 +1,7 @@
+for i in range(1,4):
+    with open(f"node{i}/dump.txt","w") as file:
+        file.write("")
+    with open(f"node{i}/log.txt","w") as file:
+        file.write("")
+    with open(f"node{i}/meta.txt","w") as file:
+        file.write("")
